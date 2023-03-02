a7711d92d2469cedf54619c7de4f65c92f47f5a3
devtest686 <126768992+devtest686@users.noreply.github.com>
2023-03-02 17:13:47 +0700

    replace

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,4 +1,4 @@
-	#!/bin/bash
+#!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
     ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnjmewhq5a0ep8 -r community-pools.mysrv.cloud:10300 -p rpc;
